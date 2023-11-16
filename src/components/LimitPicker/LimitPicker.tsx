98be4e50193dbbc32ab273f1868496ac086fc349
yurilabatsevich <yuralabatsevich@gmail.com>
2023-11-16 11:48:37 +0300

    refactor: modify LimitPicker

--- src/components/LimitPicker/LimitPicker.tsx
+++ src/components/LimitPicker/LimitPicker.tsx
@@ -1,19 +1,27 @@
 import { ChangeEvent, FC } from 'react';
-import { URLParams } from '../../types';
 import './picker.scss';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { updateLimit } from '../../store/reducers/appSlice';
+import {
+  updateLimit,
+  updatePage,
+  updateTerm,
+} from '../../store/reducers/appSlice';
+import { useSearchParams } from 'react-router-dom';
 
 export const LimitPicker: FC = () => {
+  const [searchParams, setsearchParams] = useSearchParams({});
   const search = new URL(window.location.href).searchParams;
   const { limit } = useAppSelector((state) => state.app);
   const dispatch = useAppDispatch();
 
   const onLimitChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (search.has('q')) searchParams.set('q', search.get('q') ?? '');
+    searchParams.set('limit', e.target.value);
+    searchParams.delete('page');
+    setsearchParams(searchParams);
     dispatch(updateLimit(+e.target.value));
-    const params: URLParams = {};
-    if (search.has('q')) params.q = search.get('q') ?? '';
-    params.limit = e.target.value;
+    dispatch(updatePage(1));
+    dispatch(updateTerm(searchParams.get('q') ?? ''));
   };
 
   return (
