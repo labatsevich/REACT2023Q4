5c2bcf4b895ca22f9ea5bd3782d4c568c3951db4
yurilabatsevich <yuralabatsevich@gmail.com>
2023-11-24 17:02:02 +0300

    modufy styles

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { IAnime } from "@/types";
 import Image from "next/image";
 import styles from '@/styles/pane.module.scss';
@@ -10,7 +11,7 @@ interface ICardProps {
 const Card =  ({item}: ICardProps) => {
 
   const router = useRouter();
-  const {query, pathname} = router;
+  const { query } = router;
 
   const showDetails = (id: number) => {
     router.push({query:{...query, id }})
@@ -19,7 +20,8 @@ const Card =  ({item}: ICardProps) => {
   return (
       <div className={styles.cards__item} key={item.mal_id} onClick={ () => showDetails(item.mal_id)}>
         <Image src={item.images.jpg.image_url} width={200} height={240} alt={item.title}/> 
-        <p>{item.title}</p>
+        <p className={styles.cards__item_title}>{item.title}</p>
+        <p className={styles.cards__item_year}>{item.year}</p>
       </div>
   );
 };
